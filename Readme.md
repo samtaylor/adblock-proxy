03134b46ce0bb2dc0d05d82fa90bcd132a1cce97
Sam Taylor <samueltaylor80@gmail.com>
2016-05-21 11:14:11 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#adblock-proxy
+A simple ruby proxy for blocking unwanted ads.
+
+##Blocked Servers
+This list of blocked servers is generated from [here](http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=1&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext).
---
+++ proxy.rb
@@ -0,0 +1,69 @@
+require 'webrick'
+require 'webrick/httpproxy'
+require 'set'
+
+NEWLINE = "\r\n"
+
+class AdBlockProxy < WEBrick::HTTPProxyServer
+
+  def initialize(options)
+  	super
+  	@blocked = Set.new
+  end
+
+  def load_blocked(filename)
+    logger.info("Loading #{filename}")
+  	File.open(filename).each_line do |line|
+  	  line.strip!
+  	  logger.info "#{line} already blocked" unless @blocked.add? line
+  	end
+  end
+
+  def do_GET(request, response)
+    if "adblock.proxy" == request.host
+      response.status = 200
+      response.body = "OK"
+      return
+    end
+
+    if blocked? request.host
+      logger.info "BLOCK #{request.host}"
+      response.status = 204
+      response.keep_alive = false
+    else
+      super
+    end
+  end
+
+  def do_CONNECT(request, response)
+  	host = request.header["host"].first
+  	if blocked? host
+      logger.info "BLOCK #{host}"
+  	  response.status = 204
+  	  response.keep_alive = false
+  	else
+      logger.info "ALLOW #{host}"
+  	  super
+  	end
+  end
+
+  def blocked?(host)
+  	hosts = host.split('.')
+  	while !hosts.empty?
+  		hostname = hosts.join '.'
+  		return true if @blocked.inclue?(hostname)
+  		hosts.shift
+  	end
+  	false
+  end
+
+end
+
+server = AdBlockProxy.new(:Port => 3126, :AccessLog => [])
+
+trap("INT"){server.shutdown}
+trap("TERM"){server.shutdown}
+
+server.load_blocked 'adservers'
+
+server.start
