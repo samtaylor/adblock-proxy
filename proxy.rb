37395f2a1eed3f02f5ed00c86332ffa8569c727d
Sam Taylor <samueltaylor80@gmail.com>
2016-05-21 12:07:17 +0100

    changed the script so it fetches the list of ad servers on startup added a reload command to re-fetch the ad servers

--- proxy.rb
+++ proxy.rb
@@ -1,8 +1,9 @@
 require 'webrick'
 require 'webrick/httpproxy'
 require 'set'
+require 'open-uri'
 
-NEWLINE = "\r\n"
+AD_SERVERS = "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=1&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext"
 
 class AdBlockProxy < WEBrick::HTTPProxyServer
 
@@ -12,20 +13,28 @@ class AdBlockProxy < WEBrick::HTTPProxyServer
     @running = true
   end
 
-  def load_blocked(filename)
-    logger.info("Loading #{filename}")
-  	File.open(filename).each_line do |line|
-  	  line.strip!
-  	  logger.info "#{line} already blocked" unless @blocked.add? line
-  	end
+  def load_ad_servers
+    open(AD_SERVERS) { 
+      |io| data = io.read 
+      data.each_line do |line|
+        line.strip!
+        @blocked.add line
+      end
+    }
   end
 
   def do_GET(request, response)
     if "adblock.proxy" == request.host
       if request.query['pause']
+        logger.info "AdBlock paused"
         @running = false
       elsif request.query['resume']
+        logger.info "AdBlock resumed"
         @running = true
+      elsif request.query['reload']
+        logger.info "AdBlock reload"
+        @blocked = Set.new
+        load_ad_servers
       end
       response.status = 200
       response.body = "Status: #{@running ? 'Running' : 'Paused'}"
@@ -71,6 +80,6 @@ server = AdBlockProxy.new(:Port => 8081, :AccessLog => [])
 trap("INT"){server.shutdown}
 trap("TERM"){server.shutdown}
 
-server.load_blocked 'adservers'
+server.load_ad_servers
 
 server.start
